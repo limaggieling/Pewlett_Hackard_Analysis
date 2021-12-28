f421cf6117195a96f0ed502bf15e8072b3704cd6
lingli3 <li.maggie.ling@gmail.com>
2021-12-28 05:13:00 -0500

    deliverable 2 completed

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,22 @@ INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT (ut.title) DESC;
+
+-- Deliverable 2. mentor eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+tl.title
+INTO mentorship_eligibility.csv
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as tl
+ON (e.emp_no = tl.emp_no)
+WHERE de.to_date IN ('9999-01-01') 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
+
